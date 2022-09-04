5d6708ce4a324ed0c340c61c8f37ab1fa75d1d24
hninem <hnineimoe200852@gmail.com>
2022-09-04 08:56:43 +0630

    Add Master

--- TestingProject/Program.cs
+++ TestingProject/Program.cs
@@ -1,7 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
-Console.WriteLine("Hello, World!");
+namespace BankingConsoleApp
+{
+    static class MyExtensions
+    {
+        public static int Increase(this int a)
+        {
+            return a + 1;
+        }
+        public static int Decrease(this int a)
+        {
+            return a - 1;
+        }       
+    }
+}
